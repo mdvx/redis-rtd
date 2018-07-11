390df919135295acf17f5d3d7edac191f0da3b4c
Marc Deveaux <mdvx@users.noreply.github.com>
2018-07-11 18:32:27 -0400

    re-org

--- register.cmd
+++ register.cmd
@@ -1,6 +1,6 @@
 @echo off
 
 echo Registering RTD Server
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe .\redis-rtd\bin\Release\RedisRTD.dll /codebase
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe .\rtd-client\bin\Release\RedisRTD.dll /codebase
 
 if errorlevel 1 pause
--- register.debug.cmd
+++ register.debug.cmd
@@ -1,6 +1,6 @@
 @echo off
 
 echo Registering RTD Server
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe .\Redis-rtd\bin\Debug\RedisRTD.dll /codebase
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe .\rtd-client\bin\Debug\RedisRTD.dll /codebase
 
 if errorlevel 1 pause
